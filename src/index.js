445924501c1274c7986a07cdebf09752907d6ed4
Oleg <o1e@ya.ru>
2018-11-15 11:47:14 +0300

    migrate to webpack step 3: correct use import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import vkbeautify from './js/vkbeautify';
+import vkbeautify from 'vkbeautify';
 import $ from './js/jquery-3.3.1.min';
 import './css/bootstrap.min.css';
 import './css/sql.css';
@@ -6,6 +6,9 @@ import './css/sql.css';
 
 
 const errorMessage = 'Пустой запрос';
+
+
+console.log('Loaded');
 $(document).ready(function () {
     $('#main').removeClass('d-none');
 });
@@ -19,21 +22,18 @@ export function copyFormattedSql() {
     document.execCommand('copy');
 }
 
-export function formatSql() {
-
-    sqlArea = $('#sql');
-    errorP = $('#error');
-
-    sourceSql = sqlArea.val();
-    spacesSelect = $("#spaces").find("option:selected").text();
-
-    if (sourceSql !== '') {
-        errorP.text('');
-        sql = vkbeautify.sql(sourceSql); // pretty print SQL
-        sqlArea.val(sql);
-        $('#copy').removeClass('d-none');
-    } else {
+window.formatSql = function () {
+    let sqlArea = $('#sql');
+    let errorP = $('#error');
+    let spacesSelect = $("#spaces").find("option:selected").text();
+    let sourceSql = sqlArea.val();
+    if (sourceSql.trim() === '') {
         errorP.text(errorMessage);
         $('#copy').addClass('d-none');
+    } else {
+        errorP.text('');
+        let sql = vkbeautify.sql(sourceSql); // pretty print SQL
+        sqlArea.val(sql);
+        $('#copy').removeClass('d-none');
     }
-}
+};
--- src/js/sql.js
+++ src/js/sql.js
@@ -1,2 +1,1 @@
-const errorMessage = 'Пустой запрос';
 
